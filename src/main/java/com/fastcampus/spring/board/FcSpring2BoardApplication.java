c160003d0c39c2b5844ded93ada1916dfab4aa44
p-yo00 <78263177+p-yo00@users.noreply.github.com>
2023-07-09 09:10:34 +0900

    #7 - diagram파일 revert
    이게 뭐지...
    revert해서 svg파일 바뀐거 삭제..?

---
+++ src/main/java/com/fastcampus/spring/board/FcSpring2BoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.spring.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcSpring2BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcSpring2BoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/spring/board/FcSpring2BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.spring.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcSpring2BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
